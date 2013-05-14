1e54beb7bcc15a893e9590fb27cbf08853da5599
Ben Konrath <ben@bagu.org>
2013-05-14 17:02:44 -0400

    Replace setup.py with more standard version.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 # django-salesforce
 #
 # by Phil Christensen
@@ -5,69 +8,37 @@
 # See LICENSE.md for details
 #
 
-from __future__ import with_statement
+from distutils.core import setup
+from setuptools import find_packages
 
-import distribute_setup
-distribute_setup.use_setuptools()
+import os
 
-import os, os.path, subprocess
+def get_long_description():
+    path = os.path.join(os.path.dirname(__file__), 'README.rst')
+    with open(path) as f:
+        return f.read()
 
-# disables creation of .DS_Store files inside tarballs on Mac OS X
-os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
-os.environ['COPYFILE_DISABLE'] = 'true'
+setup(
+    name='django-salesforce',
+    version='0.1.6.3',
+    license='MIT',
+    description='a Salesforce backend for Django\'s ORM',
+    long_description=get_long_description(),
+    url='https://github.com/freelancersunion/django-salesforce',
 
-def relative_path(path):
-	return os.path.join(os.path.dirname(__file__), path)
+    author='Freelancers Union',
+    author_email='devs@freelancersunion.org',
+    maintainer='Phil Christensen',
+    maintainer_email='phil@bubblehouse.org',
 
-def get_tagged_version():
-	if(os.path.exists(relative_path('VERSION'))):
-		with open(relative_path('VERSION'), 'rU') as f:
-			version = f.read().strip()
-	else:
-		proc = subprocess.Popen(['git', 'describe', '--tags'],
-			stderr	= subprocess.PIPE,
-			stdout	= subprocess.PIPE,
-			cwd		= os.path.dirname(__file__) or None
-		)
-		(stdoutdata, stderrdata) = proc.communicate()
-		if(proc.returncode):
-			raise RuntimeError(stderrdata)
-		version = stdoutdata.strip().lstrip('v')
-		
-		print "writing version file..."
-		with open(relative_path('VERSION'), 'w') as f:
-			f.write(version)
-	print 'package version: %s' % version
-	return version
-
-def autosetup():
-	from setuptools import setup, find_packages
-	return setup(
-		name			= "django-salesforce",
-		version			= get_tagged_version(),
-		
-		include_package_data = True,
-		zip_safe		= False,
-		packages		= find_packages(),
-		
-		entry_points	= {
-			'setuptools.file_finders'	: [
-				'git = setuptools_git:gitlsfiles',
-			],
-		},
-		
-		install_requires = open(relative_path('requirements.txt'), 'rU'),
-		
-		# metadata for upload to PyPI
-		author			 = "Freelancers Union",
-		author_email	 = "devs@freelancersunion.org",
-		maintainer		 = "Phil Christensen",
-		maintainer_email = "phil@bubblehouse.org",
-		description		 = "a Salesforce backend for Django's ORM",
-		license			 = "MIT",
-		keywords		 = "django salesforce orm backend",
-		url				 = "https://github.com/freelancersunion/django-salesforce",
-	)
-
-if(__name__ == '__main__'):
-	dist = autosetup()
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django>=1.3,<1.4.99',
+        'restkit>=4.1.2',
+        'simplejson>=2.5.0',
+        'oauth2>=1.5.211',
+        'pytz',
+    ],
+)
